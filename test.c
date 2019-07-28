cde9ca07a7fae0bb3cd2fa73ede529fb1f4e4247
Andy Smith <andysmit@uvic.ca>
2019-07-27 20:47:59 -0700

    added a loop to read through the image and get the hex value of the pixel.

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include <libattopng.h>
 #include <inttypes.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 #define W 255
 #define H 255
@@ -23,40 +23,49 @@ int main(int argc, char *argv[]) {
     libattopng_set_palette(png, palette, 4);
 
     int x, y;
-    for (y = 0; y < H; y++) {
-        for (x = 0; x < W; x++) {
-            libattopng_set_pixel(png, x, y, (x % 16) / 4);
-        }
-    }
-
-    libattopng_save(png, "test_palette.png");
-    libattopng_destroy(png);
-
-    // -----------------
-
-    png = libattopng_new(W, H, PNG_RGBA);
-
-    for (y = 0; y < H; y++) {
-        for (x = 0; x < W; x++) {
-            libattopng_set_pixel(png, x, y, RGBA(x & 255, y & 255, 128, (255 - ((x / 2) & 255))));
-        }
-    }
-    libattopng_save(png, "test_rgba.png");
-    libattopng_destroy(png);
-
-    // -----------------
-
     png = libattopng_new(W, H, PNG_RGB);
 
     for (y = 0; y < H; y++) {
         for (x = 0; x < W; x++) {
             libattopng_set_pixel(png, x, y, RGB(x & 255, y & 255, 128));
-	    printf("%" PRIx32 ",", RGB(x & 255, y & 255, 128) );
+//printf("%" PRIx32 ",", RGB(x & 255, y & 255, 128) );
         }
-	printf("\n");
+//	printf("\n");
     }
     libattopng_save(png, "test_rgb.png");
+    //uint32_t rgbhex;
+    uint32_t rgb[H][W];
+    //char rgb[H][W][3];
+        //FILE *test = fopen("rgblist.txt", "w");
+	  //  if (test == NULL){
+	//	                exit(1);
+	//			    }
+        for (y = 0; y < H; y++) {
+		for (x = 0; x < W; x++){
+			//fwrite(&p, PRIx32, 1, libattopng_get_pixel(png, x, y));
+			//rgbhex = libattopng_get_pixel(png, x, y);
+			rgb[y][x] =  libattopng_get_pixel(png, x, y);
+			//rgb[y][x][0] = rgbhex >> 16;
+			//rgb[y][x][1] = rgbhex >> 8;
+			//rgb[y][x][2] = rgbhex;
+			//fwrite(&rgb[y][x], sizeof(rgb[y][x]), 1, test);
+			//printf("%d,", rgbhex >> 16);
+			printf("%" PRIx32, (rgb[y][x] >> 16)& 0xFF );
+//	printf(",%02x", (char)((rgb[y][x] >> 16)));
+			if (x != (W-1)) {
+				printf(",");
+			}
+		}
+		printf("\n");
+	}
     libattopng_destroy(png);
+    /*FILE *test = fopen("rgblist.txt", "w");
+    if (test == NULL){
+	    exit(1);
+    }
+    fwrite(rgb, sizeof(uint32_t), 2, test);*/
+    //fclose(test);
+
 
     return 0;
 }

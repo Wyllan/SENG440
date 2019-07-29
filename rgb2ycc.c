cb1a33549b168b99e6dc3edf22b06f159e909aa9
Andy Smith <andysmit@uvic.ca>
2019-07-29 05:42:09 -0700

    parsed the rgb value from the list

--- rgb2ycc.c
+++ rgb2ycc.c
@@ -13,24 +13,34 @@ int main(int argc, char *argv[]) {
 		perror("NO such file\n");
 		exit(EXIT_FAILURE);
 	}
-	int rgb[3];
+	char* rgb[W][H][BGR];
 	char* token_comma;
+//	char* token_space;
+	char* end_comma;
 	char line[W*H];
-	while (fgets(line, sizeof line, fp) != NULL){
-		char *string=line;
-		token_comma = strtok(string, ",");
-		while (token_comma != NULL) {
-			/*char *token_space = strtok(token_comma, " ");
-			while (token_space != NULL) {
+	int i=0;
+       	int j=0;
+	int k=0;
+	while (fgets(line, sizeof line, fp) != NULL || i < W){
+		token_comma = strtok_r(line, ",", &end_comma);
+		while (token_comma != NULL || j < H) {
+			char *end_space;
+			char *token_space = strtok_r(token_comma, " ", &end_space);
+			//printf("%s\n", token_space);
+			while (token_space != NULL || k < BGR) {
+				//rgb[i][j][k] = token_space;
 				printf("%s ", token_space);
-				token_space = strtok(NULL, " ");
+				token_space = strtok_r(NULL, " ", &end_space);
+				k++;
 			}
-			printf("\n");*/
-			printf("%s\n", token_comma);
-			token_comma = strtok(NULL, ",");
+			printf("\n");
+			//printf("%s\n", token_comma);
+			token_comma = strtok_r(NULL, ",", &end_comma);
+			j++;
 		}
 //		printf("%s", token);
 //		token = strtok(NULL, ",");
+		i++;
 
 	}
 
@@ -46,14 +56,14 @@ int main(int argc, char *argv[]) {
 
 
 
-	int i,j,k;
+	/*int i,j,k;
 	for (i=0;i<W;i++){
 		for (j=0;j<H;j++) {
 			for (k=0;k<BGR;k++){
 				//	
 			}
 		}
-	}
+	}*/
 	fclose(fp);
 	return 0;
 }

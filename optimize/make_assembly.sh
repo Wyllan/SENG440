f3b0ded75095c80449d9946262f933c38b5acb0d
Andy Smith <andysmit@uvic.ca>
2019-08-05 14:57:04 -0700

    added assembly and yccmatrix to optimize

---
+++ optimize/make_assembly.sh
@@ -0,0 +1,2 @@
+make -f CMakeFiles/optimized_rgb2ycc.dir/build.make CMakeFiles/optimized_rgb2ycc.dir/rgb2ycc.c.s
+make -f CMakeFiles/optimized_ycc2rgb.dir/build.make CMakeFiles/optimized_ycc2rgb.dir/ycc2rgb.c.s

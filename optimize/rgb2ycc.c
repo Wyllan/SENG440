4e6988bc2f1b43e6e0e31e9f492e7fb5f624cb94
Wyllan <wyll.brimacombe@gmail.com>
2019-08-06 14:29:09 -0700

    optimize rgb2ycc

--- optimize/rgb2ycc.c
+++ optimize/rgb2ycc.c
@@ -3,10 +3,8 @@
 #include <stdlib.h>
 #include <libattopng.h>
 
-#define W 255
-#define H 255
-#define H_down 63
-#define W_down 63
+#define N 255
+#define N_half 127
 #define BGR 3
 #define RGBA(r, g, b, a) ((r) | ((g) << 8) | ((b) << 16) | ((a) << 24))
 #define RGB(r, g, b) RGBA(r, g, b, 0xff)
@@ -18,15 +16,16 @@ int main(int argc, char *argv[]) {
 		perror("NO such file\n");
 		exit(EXIT_FAILURE);
 	}
-	unsigned int rgb[W][H][BGR];
-	unsigned int ycc[W][H_down][BGR];
+	unsigned int rgb[N][N][BGR];
+	unsigned int ycc[N][N][BGR];
 	char *token_comma, *end_comma;
-	char line[W*H];
+	char line[N*N];
 	int i, j, k;
-	for(i = 0; (fgets(line, sizeof line, fp) != NULL) || i < W; i++){
+    
+	for(i = 0; (fgets(line, sizeof line, fp) != NULL) || i < N; i++){
 		char *newline=strtok(line,"\n");
 		token_comma = strtok_r(newline, ",", &end_comma);
-		for(j = 0; (token_comma != NULL) || j < H; j++) {
+		for(j = 0; (token_comma != NULL) || j < N; j++) {
 			char *end_space;
 			char *token_space = strtok_r(token_comma, " ", &end_space);
 			for (k = 0; (token_space != NULL) || k < BGR; k++) {
@@ -38,17 +37,14 @@ int main(int argc, char *argv[]) {
 	}
 	fclose(fp);
 
-    int temp;
+    int temp_i, temp_j;
     uint8_t r, g, b;
-    for(i = W; i != 0; i--) {
-        for (j = H; j != 0; j--) {
+    for(i = N; i; i--) {
+        for (j = N; j; j--) {
             r = rgb[i][j][0];
             g = rgb[i][j][1];
             b = rgb[i][j][2];
 
-            // ycc[i][j][0] = 16 + (16843*r + 33030*g + 6423*b ) >> 16;
-            // ycc[i][j][1] = 128 + (-9699*r - 19071*g + 28770*b ) >> 16;
-            // ycc[i][j][2] = 128 + (28770*r - 24117*g - 4653*b ) >> 16;
 
             ycc[i][j][0] = (16000 + 257*r + 504*g + 98*b) / 1000;
             if(ycc[i][j][0] < 16)  {
@@ -58,34 +54,35 @@ int main(int argc, char *argv[]) {
                 ycc[i][j][0] = 235;
             }
 
-            if(j % 4 == 3) {
-                temp = j/4;
+            if(i % 2 == 0 || j % 2 == 0) {
+                temp_i = i/2;
+                temp_j = j/2;
 
-                ycc[i][temp][1] = (128000 - 148*r - 291*g + 439*b) / 1000;
-                if(ycc[i][temp][1] < 16)  {
-                    ycc[i][temp][1] = 16;
+                ycc[temp_i][temp_j][1] = (128000 - 148*r - 291*g + 439*b) / 1000;
+                if(ycc[temp_i][temp_j][1] < 16)  {
+                    ycc[temp_i][temp_j][1] = 16;
                 }
-                else if (ycc[i][temp][1] > 240){
-                    ycc[i][temp][1] = 240;
+                else if (ycc[temp_i][temp_j][1] > 240){
+                    ycc[temp_i][temp_j][1] = 240;
                 }
-                ycc[i][temp][2] = (128000 + 439*r - 368*g - 71*b) / 1000;
-                if(ycc[i][temp][2] < 16)  {
-                    ycc[i][temp][2] = 16;
+                ycc[temp_i][temp_j][2] = (128000 + 439*r - 368*g - 71*b) / 1000;
+                if(ycc[temp_i][temp_j][2] < 16)  {
+                    ycc[temp_i][temp_j][2] = 16;
                 }
-                else if (ycc[i][temp][2] > 240){
-                    ycc[i][temp][2] = 240;
+                else if (ycc[temp_i][temp_j][2] > 240){
+                    ycc[temp_i][temp_j][2] = 240;
                 }
             }
         }
     }
 
 
-    libattopng_t *png = libattopng_new(W, H_down, PNG_RGB);
+    libattopng_t *png = libattopng_new(N_half, N_half, PNG_RGB);
 
 	int p,q;
 
-	for (p = 0; p < W; p++) {
-		for (q = 0; q < H_down; q++) {
+	for (p = N_half; p; p--) {
+		for (q = N_half; q; q--) {
             libattopng_set_pixel(png, p, q, RGB(ycc[p][q][0] & 255, ycc[p][q][1] & 255, ycc[p][q][2] & 255));
 		}
 	}
